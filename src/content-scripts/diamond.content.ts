208d891b61b0529b74aedaf1cfb41a6596489090
beeinger <sulecki.maciek@gmail.com>
2024-12-19 13:59:20 +0100

    diamond vip

--- src/content-scripts/diamond.content.ts
+++ src/content-scripts/diamond.content.ts
@@ -1,4 +1,22 @@
-console.log("✅ diamond.content.ts");
+async function renderDiamondUI() {
+  const address = window.location.pathname.split("/")[2];
+  const tab = window.herodotus.routeObserver?.currentTab;
 
-const address = window.location.pathname.split("/")[2];
-console.log("✅ address", address);
+  switch (tab) {
+    case "readContract":
+      console.log("✅ readContract", address, tab);
+      break;
+    case "writeContract":
+      console.log("✅ writeContract", address, tab);
+      break;
+    default:
+      break;
+  }
+}
+
+document.addEventListener(
+  "htabchange" as keyof DocumentEventMap,
+  ((_: CustomEvent<{ currentTab: string }>) => {
+    renderDiamondUI();
+  }) as EventListener
+);
