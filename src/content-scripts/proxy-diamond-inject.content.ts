5fbe515080a4bdb5c2caca9baebf9b9703dfda94
Filip Krawczyk <kotmawszy@gmail.com>
2024-12-19 21:52:57 +0100

    Implement diamond contract tabs in contract view

--- src/content-scripts/proxy-diamond-inject.content.ts
+++ src/content-scripts/proxy-diamond-inject.content.ts
@@ -1,6 +1,6 @@
 ;(function injectScript() {
     const script = document.createElement('script')
-    script.src = chrome.runtime.getURL('injected.js')
+    script.src = chrome.runtime.getURL('/proxy-diamond-inject.js')
     script.onload = () => script.remove()
     ;(document.head || document.documentElement).appendChild(script)
 })()
